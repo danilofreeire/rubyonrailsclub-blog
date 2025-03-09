4a875e7251b8e4c177ec5650f29772b187003f37
Danilo <danilofreirea90@gmail.com>
2025-03-09 12:53:15 -0300

    (has_many

--- app/controllers/administrate/categories_controller.rb
+++ app/controllers/administrate/categories_controller.rb
@@ -41,7 +41,7 @@ module Administrate
     def update
       respond_to do |format|
         if @category.update(category_params)
-          format.html { redirect_to(administrate_category_url(@category), notice: "Artigo atualizado com sucesso!") }
+          format.html { redirect_to(administrate_category_url(@category), notice: "Categoria atualizado com sucesso!") }
           format.json { render(:show, status: :ok, location: @category) }
         else
           format.html { render(:edit, status: :unprocessable_entity) }
@@ -55,7 +55,7 @@ module Administrate
       @category.destroy!
 
       respond_to do |format|
-        format.html { redirect_to(administrate_category_url, notice: "Artigo apagado com sucesso!") }
+        format.html { redirect_to(administrate_category_url, notice: "Categoria apagado com sucesso!") }
         format.json { head(:no_content) }
       end
     end
