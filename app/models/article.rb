16719e6827b545dc333ef9499171675bd4b8af92
Danilo <danilofreirea90@gmail.com>
2025-03-09 13:53:51 -0300

    Criando um model para os Autores

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,7 @@ class Article < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   belongs_to :category
+  belongs_to :author
 
   has_one_attached :cover_image do |attachable|
     attachable.variant(:thumb, resize_to_limit: [325, 205])
