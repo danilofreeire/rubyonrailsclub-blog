a4a389ce3653c358e75aff3bc2d56ad83960da61
Danilo <danilofreirea90@gmail.com>
2025-03-09 12:38:08 -0300

    Adicionando o CRUD no admin para as categorias (index)

---
+++ app/controllers/administrate/categories_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Administrate
+  class CategoriesController < AdministrateController
+
+    def index
+      @categories = Category.all
+    end
+
+  end
+end
