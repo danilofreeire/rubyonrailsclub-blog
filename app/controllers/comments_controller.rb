c28e8464638fbdd748198bcc169ec5bf4ee73034
Danilo <danilofreirea90@gmail.com>
2025-03-10 11:41:26 -0300

    Cadastrando novos comentários a partir do artigo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
- 
-class CommentsController < ApplicationController
-  def index
-    @article = Article.find(params[:article_id])
-    @comments = @article.comments
 
-    p(@comments)
-    p("*" * 100)
+class CommentsController < ApplicationController
+  def create
+    @article = Article.friendly.find(params[:article_id])
+    @article.comments.create!(comments_params)
+
+    redirect_to(article_path(@article))
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:body)
   end
 end
