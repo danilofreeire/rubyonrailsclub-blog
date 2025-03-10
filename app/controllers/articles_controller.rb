c7f3bc41983788189337e0aba7963a4c7c307730
Danilo <danilofreirea90@gmail.com>
2025-03-10 08:30:08 -0300

    Finalizando o rodapé

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,6 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.includes(:category, :author).friendly.find(params[:id])
     @other_articles = Article.all.sample(3)
+    
   end
 end
