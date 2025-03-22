5e4a9476d0e42c7a272544e27b58c54c538937b6
Danilo <danilofreirea90@gmail.com>
2025-03-22 11:36:36 -0300

    :)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@
 
 class WelcomeController < ApplicationController
   def index
+    
     @articles = Article.includes(:category, :author).all.order(updated_at: :desc).limit(6)
     @categories = Category.all.limit(8)
   end
